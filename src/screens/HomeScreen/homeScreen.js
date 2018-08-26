52f20721f79d8b91c1a79878f265aa9d01687080
Abhishek Gangwar <you@example.com>
2018-08-27 00:20:27 +0530

    setup Project

---
+++ src/screens/HomeScreen/homeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>All Coins Working</Text>
+                <Text>This is second line</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
